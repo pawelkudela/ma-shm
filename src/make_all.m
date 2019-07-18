27c7cb273a1e4b8e520fb99f6418ae7913e5cbfc
pawelkudela <pk@imp.gda.pl>
2019-07-18 09:10:01 +0200

    random excitation

--- src/make_all.m
+++ src/make_all.m
@@ -1,18 +1,35 @@
 % prepare meshes
-automesh_delam1;
-automesh_multi_pzt_simple_added_mass;
-automesh_delam_Jochen_signals;
-automesh_undelam_Jochen_signals;
-% calculate wave propagation
-flat_shell1;
-flat_shell2;
-flat_shell3;
-flat_shell_Jochen_signals_90_damping_added_mass;
-flat_shell_Jochen_signals_90_damping_baseline;
-flat_shell_Jochen_signals_90_damping;
-% visualize numerical wavefield
-plot_Jochen_wavefield_90;
-% visualize experimental wavefield data
-Read_AcousticWavefiled_my;
-% comparison of numerical and experimental signals
-Analysis_Differential_Signal_my_damp_added_mass;
+% automesh_delam1;
+% automesh_multi_pzt_simple_added_mass;
+% automesh_delam_Jochen_signals;
+% automesh_undelam_Jochen_signals;
+% % calculate wave propagation
+% flat_shell1;
+% flat_shell2;
+% flat_shell3;
+% flat_shell_Jochen_signals_90_damping_added_mass;
+% flat_shell_Jochen_signals_90_damping_baseline;
+% flat_shell_Jochen_signals_90_damping;
+% % visualize numerical wavefield
+% plot_Jochen_wavefield_90;
+% % visualize experimental wavefield data
+% Read_AcousticWavefiled_my;
+% % comparison of numerical and experimental signals
+% Analysis_Differential_Signal_my_damp_added_mass;
+
+% random excitation
+% damage location #5
+flat_shell_Jochen_random_exc_signals4;
+flat_shell_Jochen_random_exc_signals5;
+% damage location #14
+flat_shell_Jochen_random_exc_signals6;
+flat_shell_Jochen_random_exc_signals7;
+flat_shell_Jochen_random_exc_signals8;
+flat_shell_Jochen_random_exc_signals9;
+flat_shell_Jochen_random_exc_signals10;
+% damage location #22
+flat_shell_Jochen_random_exc_signals11;
+flat_shell_Jochen_random_exc_signals12;
+flat_shell_Jochen_random_exc_signals13;
+flat_shell_Jochen_random_exc_signals14;
+flat_shell_Jochen_random_exc_signals15;
